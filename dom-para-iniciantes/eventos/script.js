cb319e113fbd31eb56e4b05ac3fccfccee852b4f
karoline <shiamosan@gmail.com>
2023-06-14 16:59:57 -0300

    events

--- dom-para-iniciantes/eventos/script.js
+++ dom-para-iniciantes/eventos/script.js
@@ -1,6 +1,18 @@
+//Callback e event
 const img = document.querySelector('img');
-
-img.addEventListener('click', () => {
+function callback(event){
     console.log('clicou');
-})
-console.log(img)
+    console.log(event);
+}
+
+img.addEventListener('click', callback)
+
+//propriedades do event
+const animaisLista = document.querySelector('.animais-lista');
+function callbackLista(event){
+    console.log(event.currentTarget);
+    console.log(event.target);
+}
+
+
+animaisLista.addEventListener('click', callbackLista);
