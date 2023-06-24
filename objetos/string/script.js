8dc7d5db24beefb08e2b0fdbbfe4276f037db3f7
Karoline <shiamosan@gmail.com>
2023-06-24 19:10:56 -0300

    metodo de strings

---
+++ objetos/string/script.js
@@ -0,0 +1,14 @@
+const comida = 'Pizza';
+const agua = new String('agua');
+
+console.log(comida.length);
+console.log(agua.length);
+
+console.log(comida[0]);
+console.log(agua[0]);
+
+const frase = 'A melhor comida';
+
+console.log(frase[frase.length-1]);
+console.log(frase.charAt(frase.length - 1))
+
