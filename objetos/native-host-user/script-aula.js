8d8196a709c6a010a9ba56943e861aec0cc0d724
Karoline <shiamosan@gmail.com>
2023-06-24 18:55:20 -0300

    native host e user

---
+++ objetos/native-host-user/script-aula.js
@@ -0,0 +1,5 @@
+if(typeof Array.from !== 'undefined'){
+    console.log('Tipo de dado encontrado')
+} else {
+    console.log('Tipo de dado não existe')
+}
---
+++ objetos/native-host-user/script.js
@@ -0,0 +1,26 @@
+// Liste 5 objetos nativos
+
+// Number
+// String
+// Boolean
+// Function
+// Array
+// Object
+
+//Liste 5 objetos do browser 
+
+//HTMLCollection
+//NodeList
+//window
+//Document
+//Location
+//Navigator
+
+//Liste 2 métodos, propriedades ou objetos presentes no Chrome que não existem no Firefox
+
+
+//chrome.runtime
+//chrome.tabs
+//chrome.storage
+//chrome.notifications
+//chrome.webRequest
