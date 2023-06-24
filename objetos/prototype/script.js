28cbfff2935cdee35a88e6870c8b723d2d10df5c
Karoline <shiamosan@gmail.com>
2023-06-24 17:28:55 -0300

    prototype exercicios

--- objetos/prototype/script.js
+++ objetos/prototype/script.js
@@ -1,23 +1,42 @@
-function Pessoa(nome, idade){
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+
+function Pessoa(nome, sobrenome, idade){
     this.nome = nome;
+    this.sobrenome = sobrenome;
     this.idade = idade;
 }
 
-Pessoa.prototype.andar = function(){
-    return this.nome +' andou';
+Pessoa.prototype.nomeCompleto = function(){
+    return (this.nome + " " + this.sobrenome);
+}
+Pessoa.prototype.perfilUsuario = function(){
+    return (`Esta é ${this.nome} ${this.sobrenome}, ela tem ${this.idade} anos de idade`);
 }
 
-Pessoa.prototype.nadar = function(){
-    return this.nome +' nadou';
-}
-const andre = new Pessoa('andré', 28);
-console.log(Pessoa.prototype);
-console.log(andre.prototype);
+const karol = new Pessoa('Karol', 'Terra', 22);
 
-const listaAnimais = ['Cachorro', 'Gato', 'Cavalo'];
+console.log(karol.nomeCompleto());
 
-const listaLi = document.querySelectorAll('li')
+// Liste os métodos acessados por 
+// dados criados com NodeList,
+// HTMLCollection, Document
 
-const listaArray = Array.prototype.slice.call(listaLi);
-console.log(listaLi);
-console.log(listaArray);
+console.log(Object.getOwnPropertyNames(NodeList.prototype));
+console.log(Object.getOwnPropertyNames(HTMLCollection.prototype));
+console.log(Object.getOwnPropertyNames(Document.prototype));
+
+// Liste os construtores dos dados abaixo
+const li = document.querySelector('li');
+console.log(li.constructor) //HTMLLIElement()
+console.log(li.click.constructor); //Function()
+console.log(li.innerText.constructor); //String()
+console.log(li.value.constructor); //Number()
+console.log(li.hidden.constructor); //Boolean()
+console.log(li.offsetLeft.constructor); //Number()
+li.click(); //undefined
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name; //String()
