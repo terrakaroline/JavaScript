a1c934f09c8f155125b3193da31f0758efacdd7b
Karoline <shiamosan@gmail.com>
2023-07-24 14:29:51 -0300

    exercicios sobre array e iteracao

--- objetos/array-iteracao/script.js
+++ objetos/array-iteracao/script.js
@@ -1,20 +1,75 @@
-const li = document.querySelectorAll('li');
+// Selecione cada curso e retorne uma array
+// com objetos contendo o título, descricao,
+// aulas e horas de cada curso
+const cursos = document.querySelectorAll('.curso');
 
-const retornoForeach = li.forEach((item, index) => {
-  item.classList.add('ativa' + index);
-});
+const arrayCursos = Array.from(cursos);
 
+const objetosCurso = arrayCursos.map((curso) => {
+    const titulo = curso.querySelector('h1').innerText;
+    const descricao = curso.querySelector('p').innerText;
+    const aulas = curso.querySelector('.aulas');
+    const horas = curso.querySelector('.horas');
+    return {
+        aula: titulo,
+        descricao: descricao,
+        aulas: aulas,
+        horas: horas
+    }
+}) 
+console.log(objetosCurso)
+// Retorne uma lista com os
+// números maiores que 100
+const numeros = [3, 44, 333, 23, 122, 322, 33];
 
-console.log(retornoForeach)
-
-carros = ['Ford', 'Fiat', 'Honda'];
-
-const novaArray = carros.map((item, index, array) =>{
-  return item.toUpperCase();
+const maiores100 = numeros.filter((numero) => {
+    return numero > 100;
 })
 
-const numeros = [2, 3, 4, 5, 6, 78];
+console.log(maiores100);
 
-const numerosX2 = numeros.map(n => n*2);
-console.log(numerosX2)
-console.log(novaArray)
+
+
+// Verifique se Baixo faz parte
+// da lista de instrumentos e retorne true
+const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
+
+const buscaBaixo = instrumentos.find((instrumento) => {
+    return instrumento == 'Baixo';
+})
+
+console.log(buscaBaixo)
+
+
+// Retorne o valor total das compras
+const compras = [
+  {
+    item: 'Banana',
+    preco: 'R$ 4,99'
+  },
+  {
+    item: 'Ovo',
+    preco: 'R$ 2,99'
+  },
+  {
+    item: 'Carne',
+    preco: 'R$ 25,49'
+  },
+  {
+    item: 'Refrigerante',
+    preco: 'R$ 5,35'
+  },
+  {
+    item: 'Quejo',
+    preco: 'R$ 10,60'
+  }
+]
+
+let total = 0;
+comprasTotal = compras.forEach((compra) => {
+    let precoLimpo = compra.preco.slice(2);
+    precoLimpo = parseFloat(precoLimpo);
+    total += precoLimpo
+});
+
+console.log(total)
