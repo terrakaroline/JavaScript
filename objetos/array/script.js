91b2eb91340d7fb6ec7e627b1bc88ccedfbda248
Karoline <shiamosan@gmail.com>
2023-07-24 13:47:42 -0300

    array

---
+++ objetos/array/script.js
@@ -0,0 +1,15 @@
+const instrumentos = ['Guitarra', 'Baixo', 'Violão'];
+const precos = [49, 99, 69, 89];
+
+const dados = [new String('Tipo 1'), ['Carro', 'Portas', {cor: 'Azul', preco: 2000}], function andar(nome) { console.log(nome) }];
+
+const carros = new Array('Ford', 'Fiat', 'Honda');
+carros[2] = 'Ferrari';
+carros[3] = 'Kia';
+carros[20] = 'Kim'
+console.log(carros)
+
+const li = document.querySelectorAll('li');
+
+const arrayLi = Array.from(li);
+console.log(arrayLi)
